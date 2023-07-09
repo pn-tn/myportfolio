889090da366a619fd65ca9ab3d83eabb53ece6c6
Aloysius Hasheem Sendad <ah.sendad@gmail.com>
2023-07-09 14:20:59 +0800

    initial menu && social links

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,20 +1,37 @@
 import './Main.css';
-
-import Home from './components/Home';
+import React, { useState } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+import Home from './components/Home';
+import Menu from './components/Menu';
+
 function Main() {
+
+	const [isMenuOpen, setMenuOpen] = useState(false);
+	const toggleMenu = () => {
+		setMenuOpen(!isMenuOpen);
+	}
+	
 	return (
 		<div className="Main">
-			<FontAwesomeIcon icon={faBars} className="menu_bars" />
+			<FontAwesomeIcon icon={faBars} className="menu_bars" onClick={ toggleMenu } />
 			<div className="social">
-				<FontAwesomeIcon icon={faFacebook} className="facebook_icon" />
-				<FontAwesomeIcon icon={faGithub} className="github_icon" />
-				<FontAwesomeIcon icon={faLinkedin} className="linkedin_icon" />
+				<a href="https://web.facebook.com/asendad" target="_blank" rel="noopener noreferrer">
+					<FontAwesomeIcon icon={faFacebook} className="facebook_icon" />
+				</a>
+				<a href="https://github.com/pn-tn" target="_blank" rel="noopener noreferrer">
+					<FontAwesomeIcon icon={faGithub} className="github_icon" />
+				</a>
+				<a href="https://www.linkedin.com/in/aloysius-hasheem-sendad-5ab598259/" target="_blank" rel="noopener noreferrer">
+					<FontAwesomeIcon icon={faLinkedin} className="linkedin_icon" />
+				</a>
 			</div>
+			{isMenuOpen && (
+				<Menu />
+			)}
     	</div>
 	);
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './Menu.css';
+
+function Menu() {
+
+
+	return (
+		<div className="menu">
+			<h1>menu</h1>
+		</div>
+	);
+}
+
+export default Menu;
