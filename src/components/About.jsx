ccf851a8015f171336681f51d020c67da39b3e2e
Aloysius Hasheem Sendad <ah.sendad@gmail.com>
2023-07-12 17:23:50 +0800

    initial About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import './About.css';
+import Contacts from './Contacts';
+
 function About() {
 	return (
-		<div>
-			<h1>About</h1>
+		<div className="about">
+			<h1 className="header">Get To Know Me!</h1>
+
+			<div className="introduction">
+				<p>Hi! I am</p>
+				<p className="name">Aloysius Hasheem A. Sendad</p>
+				<p>
+					A passionate Computer Science student currently in my final year of studies, 
+					with a deep fascination for technology and a drive to solve complex problems.
+				</p>
+
+			</div>
+
+			<div className="goals">
+			</div>
+
+			<div className="personal">
+				<h1>Personal Interests</h1>
+			</div>
+			<Contacts/>
+
 		</div>
 	)
 }
