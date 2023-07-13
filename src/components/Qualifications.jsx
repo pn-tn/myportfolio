291d98d98931556b28fcfa61eecfa64acb24d5a6
Aloysius Hasheem Sendad <ah.sendad@gmail.com>
2023-07-13 09:52:14 +0800

    added the Relevant Accomplishments

--- src/components/Qualifications.jsx
+++ src/components/Qualifications.jsx
@@ -12,6 +12,16 @@ function Qualifications() {
 			</div>
 			<div className="accomplishments">
 				<h1>Relevant Accomplishments</h1>
+				<div className="accomplishment_card">
+					<h2>Leadership Award</h2>
+					<p className="org">Philippine Air force - Air Force Reserve Command</p>
+					<p className="year">2019</p>
+				</div>
+				<div className="accomplishment_card">
+					<h2>AFRC Commander's Award</h2>
+					<p className="org">Philippine Air force - Air Force Reserve Command</p>
+					<p className="year">2018</p>
+				</div>
 			</div>
 			<div className="skills">
 				<h1>Technical Skills</h1>
