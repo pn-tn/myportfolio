9ef24bc05291a11ae0dedc83b0465531387398a1
Aloysius Hasheem Sendad <ah.sendad@gmail.com>
2023-07-15 18:30:44 +0800

    added Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import './Projects.css';
+import Portfolio from './images/thisPortfolio.png';
+import Contacts from './Contacts';
+
+
+function Template({ imageSrc, title, description }) {
+	return (
+		<div className="project_template">
+			<div className="project_image">
+				<img src={imageSrc} className="image" />	
+			</div>
+			<h2>{title}</h2>
+			<p>{description}</p>
+		</div>
+	);
+}
 
 function Projects() {
+	
 	return (
-		<div>
-			<h1>Projects</h1>
+		<div className="projects_container">
+			<div className="projects">
+				<h1>Projects</h1>
+				<p>A list of small personal projects throughout my coding journey</p>
+
+				<Template 
+					imageSrc = {Portfolio}
+					title="This Portfolio :D"
+					description="A simple portfolio to showcase myself"
+				/>
+			</div>
+			<Contacts/>
+
 		</div>
-	)
+	);
 }
 
 export default Projects;
