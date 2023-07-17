d39def149e03e68e079949f9544a8db2baddce8a
Aloysius Hasheem Sendad <ah.sendad@gmail.com>
2023-07-17 18:16:46 +0800

    added Personal Interests

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,6 +23,31 @@ function About() {
 
 			<div className="personal">
 				<h1>Personal Interests</h1>
+				<p>
+					Beyond technical and academic pursuits, I also find joy in engaging with 
+					creative hobbies such as...
+				</p>
+				<div className="music-art">
+					<div className="image"></div>
+					<div className="descr">
+						<h2>Music & Art</h2>
+						<p>
+							I really love strumming on the guitar and sketching with my drawing tools. 
+							It brings me so much joy to play music and create art!
+						</p>
+					</div>	
+				</div>
+				<div className="gaming">
+					<div className="descr">
+						<h2>Gaming</h2>
+						<p>
+							I have a great time playing all kinds of games, from indie video games to 
+							tabletop role-playing games (TTRPGs). It's so much fun exploring different 
+							virtual worlds and diving into imaginative adventures. 
+						</p>
+					</div>	
+					<div className="image"></div>
+				</div>
 			</div>
 			<Contacts/>
 
