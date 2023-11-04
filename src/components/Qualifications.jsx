51a720f7be006e2232337c2fb4e48ae9127050f8
Aloysius Hasheem Sendad <ah.sendad@gmail.com>
2023-11-04 20:53:39 +0800

    added Experience and Affiliations section

--- src/components/Qualifications.jsx
+++ src/components/Qualifications.jsx
@@ -11,7 +11,7 @@ function Qualifications() {
 				<div className="education_card">
 					<h2>BS in Computer Science</h2>
 					<p className="org">Xavier University - Ateneo de Cagayan</p>
-					<p className="year">2020 - present</p>
+					<p className="year">present - 2024</p>
 				</div>
 				<div className="education_card_right">
 					<h2>Introduction to Cybersecurity</h2>
@@ -20,6 +20,40 @@ function Qualifications() {
 				</div>
 
 			</div>
+
+			<div className="experience">
+				<h1>Experience and Affiliations</h1>
+				<div className="experience_card_right">
+					<h2>Wilshire Financial Network</h2>
+					<p className="title">Intern</p>
+					<p className="year">2023</p>
+					<p className="description">
+
+						Assisted in data gathering and lead generation. <br /><br />
+						Collaborated with the team to contribute to a project's success. <br /><br />
+						Gained a better understanding of industry-specific knowledge and practices. <br /><br />
+						Gained valuable experience in effectively working remotely, leveraging digital 
+						tools and online collaboration platforms to maintain productivity. <br /><br />
+					</p>
+				</div>
+				<div className="experience_card">
+					<h2>Xavier University - PAF ROTC Officer</h2>
+					<p className="title">Training Officer</p>
+					<p className="year">2019 - 2020</p>
+					<p className="description">
+						Designed and delivered comprehensive training programs to equip and 
+						empower trainees in the Cadet Officer Qualifying Course (COQC) with
+						the necessary skills and leadership.
+					</p>
+					<p className="title">Safety Officer</p>
+					<p className="year">2018 - 2019</p>
+					<p className="description">
+						Managed and maintained medical supplies readiness, serving as the 
+						on-call point of contact for emergencies.
+					</p>
+				</div>
+			</div>
+
 			<div className="accomplishments">
 				<h1>Relevant Accomplishments</h1>
 				<div className="accomplishment_card">
